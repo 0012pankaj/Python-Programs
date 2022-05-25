aeff651c8d8ab111af16dcaac79d55a259c4262f
0012pankaj <103897354+0012pankaj@users.noreply.github.com>
2022-05-26 03:51:13 +0530

    Add files via upload

---
+++ basicifelse.py
@@ -0,0 +1,63 @@
+#1. Program to find whether an input number is even or odd.
+
+# x=int(input("enter number="))
+# if(x%2==0):
+#  print("even")
+# else:
+#  print("odd")       
+
+#2. Program to input two numbers and subtract the smaller number from the greater number.
+# x=int(input("x="))
+# y=int(input("y="))
+# if(x>y):
+#     print(x-y)
+# else:    
+#     print(y-x)
+
+#  Write a program to find maximum between two numbers. 
+# x=int(input("x="))
+# y=int(input("y="))
+# z=int(input("z=")
+# print("maximum number=",max(x,y,z))
+# print("maximum number=",min(x,y,z))
+
+# 5. Write a  program to check whether a number is negative, positive or zero. 
+# x=int(input("enter x="))
+# if(x>0):
+#     print("positive")
+# elif(x<0):
+#     print("negative")    
+# elif(x==0):
+#     print("zero")    
+
+# 6. Write a  program to check whether a number is divisible by 5 and 11 or not. 
+# x=int(input("enter number="))
+# if(x%5 == 0 and x%11 == 0):
+#     print(" devisible")
+# else:
+#     print("Not  devisible")    
+
+# 8. Write a  program to check whether a year is leap year or not. 
+# x=int(input(" enter year="))
+# if(x%4 == 0 and x%100 != 0 or x%400==0):
+#     print("leap year")
+# else:
+#     print("Not a leap year")   
+
+
+# 10. Write a  program to input any alphabet and check whether it is vowel or consonant. 
+# x=(input(" enter charecter="))
+
+# if(x=='a' or x=='e' or x=='i' or x=='0'or x=='u' or x=='A' or x=='E' or x=='I' or x=='O' or x=='U' ):
+#     print("vowel")
+# else:
+#     print("constant")  
+
+# 9. Write a program to check whether a character is alphabet or not. 
+
+# x=(input(" enter charecter="))
+
+# if(x>='a' and x<='z' or 'A'<=x and x<='Z'):
+#     print("character")
+# else:
+#     print("not character")  
